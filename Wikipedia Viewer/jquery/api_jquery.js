3ee275748f0a23f07492352cf15d94149e2fc7a6
Yosua Richel <yosuasmjtk86@gmail.com>
2017-05-09 22:06:30 +0700

    fix bugs

--- Wikipedia Viewer/jquery/api_jquery.js
+++ Wikipedia Viewer/jquery/api_jquery.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
       $("#article-list").html(""); 
       // making article-list visible again
       $("#article-list").fadeIn(1000);
-      if ($("#query").val() === "") {
+      if ($("#q").val() === "") {
          $("#no").fadeIn(500);
       } else {
          // disable the button before getting the response from API
@@ -26,7 +26,17 @@ $(document).ready(function() {
          $("#progress-bar").animate({width:"65%"}, 2000);
          $(this).attr("disabled", true);
          // passing input text
-         searchArticle($("#query").val());
+         searchArticle($("#q").val());
+         /*$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&grnnamespace=0&prop=extracts&exlimit=max&explaintext&exintro&gsrsearch=" + $("#q").val() + "&callback=?", function(result) {
+      // button Choose For Me ready to be clicked again
+         $("#submit").attr("disabled", false);
+         if (result.hasOwnProperty("query")) {
+            $.each(result.query.pages, function(key, page){
+               var extract = page.extract.length > 464 ? page.extract.substring(0,464) + "..." : page.extract;
+               $("#article-list").append('<li><h2><a target="_blank" href="http://en.wikipedia.org/?curid=' + page.pageid + '">' + page.title + '</a></h2>' + '<p>' + extract + '</p>' + '</li>');
+               });
+            }
+          });*/
          $("#no").hide(); 
          $("#progress-bar").stop().animate({width:"100%"}, 1000, function() {
          $("#hidden").fadeOut(1000); });
